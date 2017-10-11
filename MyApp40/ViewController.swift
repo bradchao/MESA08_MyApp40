6dbe38cede2204b1bb0e1910aacaff6cdb8955d9
Brad <brad@brad.tw>
2017-10-11 12:10:44 +0800

    2017101101

--- MyApp40/ViewController.swift
+++ MyApp40/ViewController.swift
@@ -7,22 +7,41 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     let app = UIApplication.shared.delegate as! AppDelegate
     
+    @IBAction func changeValue(_ sender: Any) {
+        let usersRef = app.ref.child("users")
+        
+        let userA001Ref = usersRef.child("A001")
+        userA001Ref.setValue(["username":"newBrad","passwd":"888"])
+
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let usersRef = app.ref.child("users")
         
-//        let userA001Ref = usersRef.child("A001")
-//        userA001Ref.setValue(["username":"Brad","passwd":"123456"])
+        let userA001Ref = usersRef.child("A001")
+        userA001Ref.setValue(["username":"Brad","passwd":"123456"])
 
-        let uid = "A002"
-        usersRef.child("\(uid)/username").setValue("Brad2")
-        usersRef.child("\(uid)/passwd").setValue("111111")
+//        let uid = "A002"
+//        usersRef.child("\(uid)/username").setValue("Brad2")
+//        usersRef.child("\(uid)/passwd").setValue("111111")
 
+        userA001Ref.observe( DataEventType.value) { (data) in
+            let newdata = data.value as! [String:String]
+            print("\(newdata["username"]!):\(newdata["passwd"]!)")
+        }
+        
+        usersRef.observe(DataEventType.childAdded) {
+            (data) in
+            let newref = data.ref
+            print(newref.key)
+        }
         
         
     }
